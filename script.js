0f778f2c56ceca9637b362176d99b8f56817a820
HopeThisCodeWorks <thegiftofthekings20@gmail.com>
2026-06-29 15:28:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const TICKER = document.querySelector(".ticker-text");
+
+function loadNews() {
+  const news = localStorage.getItem("villageNews");
+
+  if (news && TICKER) {
+    TICKER.innerText = news;
+  }
+}
+
+loadNews();
