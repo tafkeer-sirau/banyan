308d26ac0f4267c11549a18bdfea96b4c7c85bbe
HopeThisCodeWorks <thegiftofthekings20@gmail.com>
2026-06-29 15:28:36 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-        function startSpeech() {
-            const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-            recognition.lang = "en-IN";
-            recognition.start();
-
-            recognition.onresult = function(event) {
-                document.getElementById("speechOutput").innerHTML = event.results[0][0].transcript;
-            }
-        }
